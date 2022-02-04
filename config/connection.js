6c789307b26c2f91944b36ea8e13c8cbdac94242
alfonsof4 <alfonsofernandez4th@gmail.com>
2022-02-03 18:49:28 -0600

    updated connection js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // import the sequalize constructor from the library
+const { Sequelize } = require('sequelize');
 const sequalize = require('sequelize');
 
 require('dotenv').config();
 
-// create connecttion to our database, pass in your mySQL information for username and password
-const sequalize = new Sequalize('just_tech_news_db', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequalize;
+
+if(process.env.JAWSDB_URL) {
+    sequalize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequalize;
+    
+   
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const userRoutes = require('./user');
+const postRoutes = require('./post');
+const commentRoutes = require('./comment');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
